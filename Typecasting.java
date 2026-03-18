36d5222bf4951bf824943a84c33ffeb3bfab980a
Navaneeth <navaneethmortha123@gmail.com>
2026-03-18 10:51:35 +0530

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,12 @@
+public class Typecasting
+{
+	public static void main(String[] args)
+	{
+	 byte b=20;
+	 int x=b;// impilicit type// converts byte into int
+	 float f=b;//converts int to float
+	 System.out.println("Byte value :"+b);
+	 System.out.println("Integer x value after type conversion :"+x);
+	 System.out.println("Float f value after type conversion :"+f);
+	}
+}
